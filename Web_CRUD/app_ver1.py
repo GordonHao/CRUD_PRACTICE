5cb780a8214c928b6de2837d6dedfb94569082f0
GordonHao <Gordon@gmail.com>
2019-01-16 20:46:39 +0800

    Web_CRUD_practice

---
+++ Web_CRUD/app_ver1.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, url_for, redirect
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '12345'
+app.config['MYSQL_DB'] = 'testdb'
+mysql = MySQL(app)
+
+@app.route('/')
+def home():
+    cur = mysql.connection.cursor()
+    cur.execute("select information.class, information.Name, grades.Subject, grades.grade from information, grades where class='甲' and subject='國文' and grades.studentid=information.id")
+    rv = cur.fetchall()
+    cur.close()
+    return render_template('home.html', classes=rv)
+
+
+@app.route('/Save', methods=["POST"])
+def Save():
+    classs = request.form['class']
+    name = request.form['name']
+    gender = request.form['gender']
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO class (class, name, gender) VALUES (%s, %s, %s)",(classs, name, gender,))
+    mysql.connection.commit()
+    return redirect(url_for('home'))
+
+@app.route('/update', methods=['POST'])
+def update():
+    Num_data = request.form['Num']
+    classs = request.form['class']
+    name = request.form['name']
+    gender = request.form['gender']
+    cur = mysql.connection.cursor()
+    cur.execute("update class SET class=%s, name=%s, gender=%s WHERE Num=%s",(classs, name, gender, Num_data,))
+    mysql.connection.commit()
+    return redirect(url_for('home'))
+
+@app.route('/delete/<string:Num_data>', methods=["GET"])
+def delete(Num_data):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM class WHERE Num=%s",(Num_data,))
+    mysql.connection.commit()
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
